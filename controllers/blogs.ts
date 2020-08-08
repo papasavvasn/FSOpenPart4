ee4a35dd4653b86ef8bcd4499bc93112cc9bdeb4
papasavvasn <papasavvasn@media-saturn.com>
2020-08-08 20:28:05 +0200

    part4b,add api tests with test db, set up different envs ex.4.8-4.14

--- controllers/blogs.ts
+++ controllers/blogs.ts
@@ -1,18 +1,32 @@
 import { Request, Response, Router } from 'express';
 import { Blog } from "../models/blog"
+import { runInNewContext } from 'vm';
 
 export const blogsRouter = Router()
 
-blogsRouter.get('/', (_: Request, res: Response) => {
-    Blog
-        .find({})
-        .then(blogs => { res.json(blogs) })
+blogsRouter.get('/', async (_: Request, res: Response) => {
+    const blogs = await Blog.find({})
+    res.json(blogs)
 })
 
-blogsRouter.post('/', (req: Request, res: Response) => {
-    const blog = new Blog(req.body)
-
-    blog.save().then(result => {
+blogsRouter.post('/', async (req: Request, res: Response) => {
+    if (!req.body.url && !req.body.title) {
+        return res.status(400).end()
+    } else {
+        const blog = new Blog({ ...req.body, likes: req.body.likes || 0 })
+        const result = await blog.save()
         res.status(201).json(result)
-    })
+    }
+})
+
+
+blogsRouter.put('/:id', async (req: Request, res: Response) => {
+    const { title, author, url, likes } = req.body
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, { title, author, url, likes }, { new: true })
+    res.json(updatedBlog)
+})
+
+blogsRouter.delete("/:id", async (req: Request, res: Response) => {
+    const deletedPost = await Blog.findByIdAndRemove(req.params.id)
+    res.status(204).end()
 })
--- utils/config.ts
+++ utils/config.ts
@@ -1,4 +1,8 @@
 //https://dev.to/aryanshmahato/setup-node-express-with-typescript-3bho
 require('dotenv').config()
-export const PORT = 3003
-export const MONGODB_URI = process.env.MONGODB_URI
+export const PORT = process.env.PORT
+
+export let MONGODB_URI = process.env.MONGODB_URI
+if (process.env.NODE_ENV === 'test') {
+    MONGODB_URI = process.env.TEST_MONGODB_URI
+}
